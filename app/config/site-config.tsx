43d730cb76553463a027580ee92157ac02027825
AngelSmithlgs <angelsmithlgs@gmail.com>
2024-03-28 13:36:58 -0300

    add 1 letter lmao

--- app/config/site-config.tsx
+++ app/config/site-config.tsx
@@ -1,7 +1,7 @@
 export const siteConfig = {
     creator: "Angel Smith",
     title: "Developer",
-    bio: "From code to cuisine: powered by tofu and frie potatoes",
+    bio: "From code to cuisine: powered by tofu and fried potatoes",
     location: "Santiago, Chile",
     locationLink: "a",
     email: "angelsmithlgs@gmail.com",
